565ac1a280ae81f6e2c19ab0177cc5458d18894d
edgarb <edgar_daniel_1996@hotmail.com>
2023-11-17 09:59:39 -0800

    add improvements on logs index

--- src/pages/persons/logs/index.js
+++ src/pages/persons/logs/index.js
@@ -8,7 +8,6 @@ export const PersonsLogsPage = ()=>{
   return(
     <div className="w-full flex flex-col items-center">
       <Link className=' mb-4 self-start' to={`/persons`}> {'<'} <span className='underline'>Regresar a Personas</span></Link>
-      <h1 className='w-full text-center mb-2 text-2xl'>Registros</h1>
       <Logs personId={personId}/>
     </div>
   )
