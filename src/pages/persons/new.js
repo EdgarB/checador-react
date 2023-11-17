2f4182e475c78909970c7b2c60070f6ed462a7d7
edgarb <edgar_daniel_1996@hotmail.com>
2023-11-17 00:25:22 -0800

    update small margin

--- src/pages/persons/new.js
+++ src/pages/persons/new.js
@@ -6,7 +6,7 @@ export const NewPersonPage = ()=>{
   return(
     <div className='flex items-center flex-col w-full'>
       <Link className=' mb-4 self-start' to={`/persons`}> {'<'} <span className='underline'>Regresar a Personas</span></Link>
-      <h1 className="w-full text-center mb-2 text-2xl">Registrar nueva persona</h1>
+      <h1 className="w-full text-center mb-4 text-2xl">Registrar nueva persona</h1>
       <NewPersonForm/>
     </div>
   )
